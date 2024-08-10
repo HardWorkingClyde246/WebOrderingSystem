218862baba6ebdb11dd756f42c1a8f53addc9777
Rui Yi <kuary-wm22@student.tarc.edu.my>
2024-08-10 16:00:44 +0800

    cartview with dummy done

--- Web-Assignment-2/CartView.aspx
+++ Web-Assignment-2/CartView.aspx
@@ -1,15 +1,45 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CartView.aspx.cs" Inherits="Web_Assignment_2.CartView" %>
+﻿<%@ Page Language="C#" MasterPageFile="~/Site1.Master" CodeBehind="CartView.aspx.cs" Inherits="Web_Assignment_2.CartView" %>
 
 
 
 <asp:Content ID="conten1"  ContentPlaceHolderID="ContentPlaceHolder1"  runat="server">
-<body>
-    <form id="form1" runat="server">
-        <div>
 
+    <div>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">Food name</td>
+                <td><asp:Label ID="lblFoodLabel" runat="server" Text="Label"></asp:Label></td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Quantity</td>
+                <td><asp:Label ID="lblQuantity" runat="server" Text="Label"></asp:Label></td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Price</td>
+                <td><asp:Label ID="lblFoodPrice" runat="server" Text="Label"></asp:Label></td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Total Price</td>
+                <td>&nbsp;</td>
+                <td> <asp:Label ID="lblTotalPrice" runat="server" Text="Label"></asp:Label></td>
+            </tr>
+        </table>
+        
+    
+    </div>
 
-
-        </div>
-    </form>
-</body>
 </asp:Content>
+<asp:Content ID="Content1" runat="server" contentplaceholderid="head">
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 148px;
+        }
+    </style>
+</asp:Content>
+
--- Web-Assignment-2/CartView.aspx.cs
+++ Web-Assignment-2/CartView.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
 
 namespace Web_Assignment_2
 {
@@ -11,6 +14,43 @@ namespace Web_Assignment_2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                SqlConnection con;
+                string constr = ConfigurationManager.ConnectionStrings["ConnectionStringA"].ConnectionString;
+                con = new SqlConnection(constr);
+                con.Open();
+
+                int orderID = 1;
+                string strGetFoodName = "select f.FoodLabel from Orders o inner join Food f on o.FoodId = f.FoodId where OrderID =" + orderID;
+                SqlCommand cmdRtr1;
+                cmdRtr1 = new SqlCommand(strGetFoodName, con);
+                string foodName = cmdRtr1.ExecuteScalar().ToString();
+                lblFoodLabel.Text = foodName;
+
+                string strGetQuantity = "select Quantity from Orders o where OrderID = " + orderID;
+                SqlCommand cmdRtr2;
+                cmdRtr2 = new SqlCommand(strGetQuantity, con);
+                string quantity = cmdRtr2.ExecuteScalar().ToString();
+                lblQuantity.Text = quantity;
+
+                string strGetFoodPrice = "select f.Price from Orders o inner join Food f on o.FoodId = f.FoodId where OrderID = " + orderID;
+                SqlCommand cmdRtr3;
+                cmdRtr3 = new SqlCommand(strGetFoodPrice, con);
+                string foodPrice = cmdRtr3.ExecuteScalar().ToString();
+                lblFoodPrice.Text = foodPrice;
+
+                string strGetTotal = "SELECT price * quantity AS total FROM orders where OrderID = " + orderID;
+
+                SqlCommand cmdRtr4;
+                cmdRtr4 = new SqlCommand(strGetTotal, con);
+                string total = cmdRtr4.ExecuteScalar().ToString();
+                lblTotalPrice.Text = total;
+
+
+                con.Close();
+
+            }
 
         }
     }
